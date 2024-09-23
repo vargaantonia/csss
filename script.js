957612d734492d26b2bc55c8aa415c3349cbfcc9
vargaantonia <vargaa@kkszki.hu>
2024-09-23 17:20:32 +0200

    vegleges

---
+++ script.js
@@ -0,0 +1,45 @@
+function generatePassword() {
+    const password = Math.random().toString(36).slice(-8); 
+    document.getElementById('password').value = password;
+}
+
+
+function validateForm() {
+    const name = document.getElementById('name').value;
+    const age = document.getElementById('age').value;
+    const email = document.getElementById('email').value;
+
+    const nameValid = /^[A-ZÁÉÍÓÖŐÚÜŰ][a-záéíóöőúüű]*$/.test(name); 
+    const ageValid = age > 0; 
+    const emailValid = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.(hu|com|net|edu)$/.test(email); 
+
+    if (!nameValid) {
+        alert('A névnek nagybetűvel kell kezdődnie!');
+        return false;
+    }
+    if (!ageValid) {
+        alert('Kérlek, add meg a korodat!');
+        return false;
+    }
+    if (!emailValid) {
+        alert('Kérlek, add meg a helyes email címet!');
+        return false;
+    }
+
+    return true;
+}
+
+function togglePasswordVisibility() {
+    const passwordInput = document.getElementById('password');
+    const toggleIcon = document.getElementById('toggleIcon');
+    
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        toggleIcon.classList.remove('bi-eye-slash');
+        toggleIcon.classList.add('bi-eye');
+    } else {
+        passwordInput.type = 'password';
+        toggleIcon.classList.remove('bi-eye');
+        toggleIcon.classList.add('bi-eye-slash');
+    }
+}
